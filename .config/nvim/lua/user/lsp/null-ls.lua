9a44a1067cff7e1c0684f566ee0fd79aa54adaaf
Stefan Bondzulic <18123522+bondzula@users.noreply.github.com>
2022-02-05 22:08:44 +0100

    [Neovim] remove spell and grammar checks from null-ls

--- .config/nvim/lua/user/lsp/null-ls.lua
+++ .config/nvim/lua/user/lsp/null-ls.lua
@@ -6,39 +6,27 @@ end
 -- For esier usage
 local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
-local code_actions = null_ls.builtins.code_actions
-
 
 null_ls.setup({
 	debug = false,
 	sources = {
-    -- General formatting
-    formatting.trim_newlines,
-    formatting.trim_whitespace,
+		-- General formatting
+		formatting.trim_newlines,
+		formatting.trim_whitespace,
 
-    -- Spelling and writing style
-    -- diagnostics.cspell,
+		-- Lua
+		formatting.stylua,
 
-    diagnostics.proselint.with({
-        extra_filetypes = { "NeogitCommitMessage" },
-    }),
-    code_actions.proselint.with({
-        extra_filetypes = { "NeogitCommitMessage" },
-    }),
+		-- Javascript (js, jsx, vue)
+		formatting.prettierd,
+		diagnostics.eslint_d,
 
-    -- Lua
-    formatting.stylua,
+		-- PHP
+		-- TODO: php_cs_fixer
+		-- TODO: phpstan https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#about-97
 
-    -- Javascript (js, jsx, vue)
-		formatting.prettier,
-    diagnostics.eslint_d,
-
-    -- PHP
-    -- TODO: php_cs_fixer
-    -- TODO: phpstan https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#about-97
-
-    -- Stylelint (css, scss, less)
-    formatting.stylelint,
-    diagnostics.stylelint,
+		-- Stylelint (css, scss, less)
+		formatting.stylelint,
+		diagnostics.stylelint,
 	},
 })
