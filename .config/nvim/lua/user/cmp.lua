1ca8a1cb529410758e235a2a961bc2cc2ed2d05a
Stefan Bondzulic <18123522+bondzula@users.noreply.github.com>
2022-04-19 15:26:18 +0200

    Change deprecated values

--- .config/nvim/lua/user/cmp.lua
+++ .config/nvim/lua/user/cmp.lua
@@ -146,9 +146,9 @@ cmp.setup({
 		behavior = cmp.ConfirmBehavior.Replace,
 		select = false,
 	},
-	documentation = {
-		border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-	},
+	-- documentation = {
+	-- 	border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+	-- },
 	experimental = {
 		ghost_text = false,
 		native_menu = false,
